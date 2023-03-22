a2d0eaee2b146930cf49c91fd5b6fa38d3b97db9
Peter Bengtsson <mail@peterbe.com>
2023-03-22 10:00:40 -0400

    rollups

--- index.js
+++ index.js
@@ -39,9 +39,63 @@ app.post("/*", (req, res) => {
     console.log("Reported into", DB_FILE);
   });
   console.log(msg);
+
+  console.log("");
+  for (const [date, counts] of rollups(db.data.errors, "message")) {
+    console.log(
+      `Rollups ${chalk.bold(date)} ${chalk.dim("(delete db.json to reset)")}`
+    );
+    for (const [value, count] of Object.entries(counts)) {
+      console.log(
+        `${chalk.yellowBright(`${count}`.padStart(3))}  ${chalk.green(value)}`
+      );
+    }
+  }
+
   res.send(msg);
 });
 
+function rollups(errors, key) {
+  const byDay = {};
+  for (const thing of errors) {
+    console.log(thing);
+    break;
+  }
+  // return [];
+  for (const { date, body } of errors) {
+    const dateStr =
+      typeof date === "string"
+        ? date.split("T")[0]
+        : date.toISOString().split("T")[0];
+    if (!(dateStr in byDay)) {
+      byDay[dateStr] = {};
+    }
+    const count = byDay[dateStr];
+    const value = body[key] || "/no value/";
+    // for (const body of ) {
+    count[value] = (count[value] || 0) + 1;
+    // }
+  }
+  return Object.entries(byDay).sort((a, b) => a[0].localeCompare(b[0]));
+}
+
+function countSchemas(events) {
+  const byDay = {};
+  for (const { date, body } of events) {
+    const dateStr =
+      typeof date === "string"
+        ? date.split("T")[0]
+        : date.toISOString().split("T")[0];
+    if (!(dateStr in byDay)) {
+      byDay[dateStr] = {};
+    }
+    const count = byDay[dateStr];
+    for (const { schema } of body.events) {
+      count[schema] = (count[schema] || 0) + 1;
+    }
+  }
+  return Object.entries(byDay).sort((a, b) => a[0].localeCompare(b[0]));
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
