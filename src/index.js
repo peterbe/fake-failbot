bf848024922439e5d84c27eae32e31151edfbe9b
Peter Bengtsson <mail@peterbe.com>
2024-01-17 11:03:29 -0500

    whole pathname

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.get("/*", (req, res) => {
   res.send(msg);
 });
 app.post("/*", (req, res) => {
-  const msg = `POSTING to ${req.url.slice(1)}`;
+  const msg = `POSTING to ${req.url}`;
   console.log("Error reporting!");
   for (const [key, value] of Object.entries(req.body || {})) {
     console.log("  ", chalk.bold(key), chalk.red(`${value}`.slice(0, 100)));
@@ -41,11 +41,11 @@ app.post("/*", (req, res) => {
   console.log("");
   for (const [date, counts] of rollups(db.data.errors, "message")) {
     console.log(
-      `Rollups ${chalk.bold(date)} ${chalk.dim("(delete db.json to reset)")}`,
+      `Rollups ${chalk.bold(date)} ${chalk.dim("(delete db.json to reset)")}`
     );
     for (const [value, count] of Object.entries(counts)) {
       console.log(
-        `${chalk.yellowBright(`${count}`.padStart(3))}  ${chalk.green(value)}`,
+        `${chalk.yellowBright(`${count}`.padStart(3))}  ${chalk.green(value)}`
       );
     }
   }
